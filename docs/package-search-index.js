8966577e1db5ca0ebba79337c9196f86475091fc
imyoric <63288807+imyoric@users.noreply.github.com>
2024-04-21 17:37:39 +0500

    Docs for 2.0_Beta-8

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.yoricya.tgauto"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.yoricya.tgsaver"}];updateSearchResults();
